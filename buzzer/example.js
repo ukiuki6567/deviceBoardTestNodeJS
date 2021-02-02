0953afbe72f42e787c9f9d39dc62a606b43ac5e6
KokiOta <ma20015@shibaura-it.ac.jp>
2021-02-02 14:18:40 +0900

    test completed

---
+++ buzzer/example.js
@@ -0,0 +1,17 @@
+const Gpio = require('pigpio').Gpio;
+
+const buzzer = new Gpio(12, {mode: Gpio.OUTPUT});
+const freq = [523, 587,659, 698, 784, 880,  988, 1046];
+
+function msleep(n){
+	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+function sleep(n){
+	msleep(n*1000);
+}
+
+for(let i=0;i<freq.length;i++){
+	buzzer.hardwarePwmWrite(freq[i], 500000);
+	sleep(1);
+}
+buzzer.pwmWrite(0);
